4241a6b3ab6236a45ba279ff7810e88c5155a46d
ChrisHorrigan <bor3d.mad@gmail.com>
2014-05-05 13:49:11 -0400

    SpaceshipCreator minor changes
    involving names and spawning

--- FinalProject/Assets/SpaceShip/SpaceshipCreator.cs
+++ FinalProject/Assets/SpaceShip/SpaceshipCreator.cs
@@ -5,14 +5,15 @@ public class SpaceshipCreator : MonoBehaviour {
 
 	public Transform spaceship;
 	public Transform planet;
-
+	public string playerName;
 	// Use this for initialization
 	void Start () {
+
 		if(Network.isServer) {
 			Transform tempShip = Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;
 			//Network.Instantiate(spaceship, transform.position, transform.rotation, 0);
 			Camera.main.transform.parent = tempShip.transform;
-		
+			playerName=GetName ();
 			tempShip.GetComponent<SpaceCode>().sendName(GetName ());
 			Network.Instantiate(planet, new Vector3(1000, 0, 0), transform.rotation, 0);
 		}
@@ -23,17 +24,23 @@ public class SpaceshipCreator : MonoBehaviour {
 	}
 
 	void OnConnectedToServer() {
-		Transform tempShip = Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;
+		Transform tempShip = spaceship;
+			//Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;//this is the real one
 		//Network.Instantiate(spaceship, transform.position, transform.rotation, 0);
 		Camera.main.transform.parent = tempShip.transform;
 		tempShip.GetComponent<SpaceCode>().sendName(GetName ());
+
 	}
 	void OnDisconnectedFromServer(){
 		Application.LoadLevel (0);
 		}
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	void RoundStart(){
+
 	}
 	string GetName(){
 		MenuScript menuInstance=GameObject.Find ("MenuManager").GetComponent<MenuScript>();
