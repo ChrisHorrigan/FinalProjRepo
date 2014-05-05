638de799cf8139f6b07e456c8fc93e8d9be46d3c
zephyrLucas <zephyr@lucas.net>
2014-05-05 13:53:16 -0400

    Flag made
    the flag has been made an is now put into the scene

--- FinalProject/Assets/SpaceShip/SpaceshipCreator.cs
+++ FinalProject/Assets/SpaceShip/SpaceshipCreator.cs
@@ -5,17 +5,18 @@ public class SpaceshipCreator : MonoBehaviour {
 
 	public Transform spaceship;
 	public Transform planet;
-	public string playerName;
+	public Transform flag1;
+
 	// Use this for initialization
 	void Start () {
-
 		if(Network.isServer) {
 			Transform tempShip = Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;
 			//Network.Instantiate(spaceship, transform.position, transform.rotation, 0);
 			Camera.main.transform.parent = tempShip.transform;
-			playerName=GetName ();
+		
 			tempShip.GetComponent<SpaceCode>().sendName(GetName ());
 			Network.Instantiate(planet, new Vector3(1000, 0, 0), transform.rotation, 0);
+			Network.Instantiate(flag1, new Vector3(0, 0, 10), transform.rotation, 0);
 		}
 
 
@@ -24,23 +25,17 @@ public class SpaceshipCreator : MonoBehaviour {
 	}
 
 	void OnConnectedToServer() {
-		Transform tempShip = spaceship;
-			//Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;//this is the real one
+		Transform tempShip = Network.Instantiate(spaceship, transform.position, transform.rotation, 0) as Transform;
 		//Network.Instantiate(spaceship, transform.position, transform.rotation, 0);
 		Camera.main.transform.parent = tempShip.transform;
 		tempShip.GetComponent<SpaceCode>().sendName(GetName ());
-
 	}
 	void OnDisconnectedFromServer(){
 		Application.LoadLevel (0);
 		}
 	// Update is called once per frame
 	void Update () {
-
-	}
-
-	void RoundStart(){
-
+	
 	}
 	string GetName(){
 		MenuScript menuInstance=GameObject.Find ("MenuManager").GetComponent<MenuScript>();
