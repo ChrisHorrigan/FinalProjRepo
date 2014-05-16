9fc67fd2fd015db3ce697cb91b61752ef846287e
zephyrLucas <zephyr@lucas.net>
2014-05-16 12:57:50 -0400

    Seekers Explode!!

--- FinalProject/Assets/SpaceShip/HeatSeekers/SeekerCode.cs
+++ FinalProject/Assets/SpaceShip/HeatSeekers/SeekerCode.cs
@@ -5,8 +5,11 @@ public class SeekerCode : DestructableObject {
 
 	public Transform tempTarget;
 	PathFinder pathFinder;
+
+	private float exploding;
 	// Use this for initialization
 	void Start () {
+		exploding = -1f;
 		//tempTarget = GameObject.Find("target").transform;
 		//pathFinder = new PathFinder(tempTarget, this.transform);
 
@@ -14,11 +17,22 @@ public class SeekerCode : DestructableObject {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!pathFinder.Equals(null)) {
-			this.GetComponent<CharacterController>().Move(10f * Time.deltaTime * pathFinder.findPath());
-		}
-		if(pathFinder.getDistance() < 5f) {
-			explode ();
+		if(exploding >= 0f) {
+			exploding += Time.deltaTime;
+			if(exploding >= .45f) {
+				GameObject[] all = GameObject.FindObjectsOfType<GameObject>();
+				foreach (GameObject stepOne in all) {
+
+				}
+				Destroy(this.gameObject);
+			}
+		} else {
+			if (!pathFinder.Equals(null)) {
+				this.GetComponent<CharacterController>().Move(10f * Time.deltaTime * pathFinder.findPath());
+			}
+			if(pathFinder.getDistance() < 5f) {
+				explode ();
+			}
 		}
 	}
 
@@ -38,11 +52,9 @@ public class SeekerCode : DestructableObject {
 	}
 
 	public void explode() {
-
-
-
-
-		Destroy(this.gameObject);
+		exploding = 0.0001f;
+		this.GetComponent<ParticleSystem>().Play();
+		//Destroy(this.gameObject);
 	}
 	
 
